54bf5da808729fee9a613ba742a2cf55ca857639
wrtzla <1664427585@qq.com>
2024-09-27 14:50:21 +0800

    Site updated: 2024-09-27 14:50:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/03/Markdown语法与外挂标签写法汇总/","2024/07/26/红米ax6000刷openwrt教程，终于有完善好用的openwrt了/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/07/26/红米ax6000刷openwrt教程，终于有完善好用的openwrt了/","2023/05/03/Markdown语法与外挂标签写法汇总/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
